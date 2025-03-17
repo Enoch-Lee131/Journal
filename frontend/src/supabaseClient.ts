bba7cda7867024020459853a101719175762b2a5
Enok <enochlee@berkeley.edu>
2025-03-16 18:13:55 -0700

    fix

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+// @ts-expect-error - Vite env variables
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "";
+// @ts-expect-error - Vite env variables
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY || "";
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
